ad5564b58974595e34b0f8c765e8635e750c0388
Mary Lee <stella-maris@users.noreply.github.com>
2015-09-08 10:56:15 -0700

    Update siege_engine.rb

--- lib/siege_engine.rb
+++ lib/siege_engine.rb
@@ -1,10 +1,9 @@
 require_relative "spec_helper"
 
 class SiegeEngine < Unit
-	attr_accessor :gold, :food, :lumber
+  attr_accessor :gold, :food, :lumber
 
-	def initialize
-		super(400, 50)
-	end
-
+  def initialize
+    super(400, 50)
+  end
 end
