5258b00a4ebab04ee5bbadb6d629d26ba2c54c84
Mary Lee <stella-maris@users.noreply.github.com>
2015-09-08 10:55:11 -0700

    Update unit.rb

--- lib/unit.rb
+++ lib/unit.rb
@@ -1,33 +1,17 @@
-require 'pry'
 class Unit
-	attr_reader :health_points, :attack_power
-	
-	def initialize(health_points, attack_power)
-		@health_points = health_points
-		@attack_power = attack_power
-	end
+  attr_accessor :attack_power
+  attr_reader :health_points
 
-	def is_dead?
-		@health_points == 0
-	end
-
- def attack!(enemy)
-    
-    x = @attack_power
-    return if enemy.is_dead?
-    # the 'enemy' is being passed in, we know that our 'enemy' is also a footman(object)
-    #this footman(enemy) can also take damage
-    #thus, we can use our #damage and pass in OUR footmans attack_power. 
-    if enemy.class.name == 'Barracks'
-      enemy.damage(@attack_power / 2)
-    elsif enemy.class.name == 'SiegeEngine'
-      enemy.damage(@attack_power * 2)
-    else enemy.damage(@attack_power)
-    end
+  def initialize(health_points, attack_power)
+    @health_points = health_points
+    @attack_power = attack_power
   end
 
-  def damage(attack_power)
-    @health_points -= attack_power
+  def attack!(enemy)
+    enemy.damage(self.attack_power) 
   end
 
+  def damage(damage_points)
+    @health_points -= damage_points
+  end
 end
